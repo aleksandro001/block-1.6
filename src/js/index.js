4b16836a8d02410d7e4bbf97f87b5777204bf0c9
aleksandro001 <001aleksandro@gmail.com>
2023-07-11 15:12:47 +0300

    add burger menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 import '../scss/style.scss'
 
 console.log('Works!')
+
+let active = document.querySelector(".burger__toggle");
+let open = document.querySelector(".burger");
+let clock = document.querySelector(".burger__button-cross");
+let block = document.querySelector(".wrap");
+
+active.addEventListener("click", function () {
+  open.classList.toggle("active");
+  block.classList.toggle("block");
+  console.log('go')
+});
+clock.addEventListener("click", function(){
+  open.classList.toggle("active");
+  block.classList.toggle("block");
+})
