c57862c6704f9a670391820278ac4a2ef7d54293
aleksandro001 <001aleksandro@gmail.com>
2023-07-15 10:46:45 +0300

    add table slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@
 import 'swiper/swiper.scss';
 import 'swiper/modules/pagination.scss';
 import '../scss/style.scss';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
 import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
 
@@ -25,11 +27,24 @@ clock.addEventListener("click", function(){
 })
 
 //===================================
-let btnExpand = document.querySelector(".swiper__show");
-let sliderIcons = document.querySelector(".swiper-wrapper");
+let btnExpand = document.querySelector(".swiper-brend__show");
+let sliderIconsBrend = document.querySelector(".swiper-brend__wrapper");
+let sliderIconsTechniqe = document.querySelector(".swiper-technique__wrapper");
+let btnExpandTech = document.querySelector(".swiper-technique__show");
 
 btnExpand.addEventListener("click", function () {
-  sliderIcons.classList.toggle("click");
+  sliderIconsBrend.classList.toggle("click");
+  this.classList.toggle("click");
+
+  if (this.classList.contains("click")) {
+    this.textContent = "Скрыть";
+  } else {
+    this.textContent = "Показать все";
+  }
+});
+
+btnExpandTech.addEventListener("click", function(){
+  sliderIconsTechniqe.classList.toggle("click");
   this.classList.toggle("click");
 
   if (this.classList.contains("click")) {
@@ -41,15 +56,33 @@ btnExpand.addEventListener("click", function () {
 
 window.addEventListener("resize", function () {
   if (window.matchMedia("(min-width: 768px)").matches) {
-    sliderIcons.style.transform = "";
+    sliderIconsBrend.style.transform = "";
+    sliderIconsTechniqe.style.transform = "";
+    swiper1.disable();
     swiper.disable();
   }else{
     swiper.enable();
+    swiper1.enable();
   }
 });
 //===================================
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper-brend", {
+  // Optional parameters
+  spaceBetween: 10,
+  direction: "horizontal",
+  loop: true,
+  width: 720,
+  slidesPerView: 3,
+  modules: [Pagination],
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+const swiper1 = new Swiper(".swiper-technique", {
   // Optional parameters
   spaceBetween: 10,
   direction: "horizontal",
@@ -66,5 +99,6 @@ const swiper = new Swiper(".swiper", {
 });
 const width = window.innerWidth
 if (width >= 768){
+  swiper1.disable();
   swiper.disable();
 }
